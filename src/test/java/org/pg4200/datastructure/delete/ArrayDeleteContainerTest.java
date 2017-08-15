3fedf3223fc666ba3793a19fd407372a5a6a78cf
arcuri82 <arcuri82@gmail.com>
2017-08-15 15:06:58 +0200

    collection with delete

---
+++ src/test/java/org/pg4200/datastructure/delete/ArrayDeleteContainerTest.java
@@ -0,0 +1,14 @@
+package org.pg4200.datastructure.delete;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by arcuri82 on 15-Aug-17.
+ */
+public class ArrayDeleteContainerTest extends DeleteContainerTestTemplate{
+
+    @Override
+    protected <T> DeleteContainer<T> getNewInstance(Class<T> klass) {
+        return new ArrayDeleteContainer<>(10);
+    }
+}
