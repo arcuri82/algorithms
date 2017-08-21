51bc4a580f921bc03d975100ff8001a1bed9565d
arcuri82 <arcuri82@gmail.com>
2017-08-21 16:04:25 +0200

    exercise 04

--- README.md
+++ README.md
@@ -6,3 +6,4 @@ Code examples and exercises for the PG4200 course: Algoritmer og datastrukturer
 * [Ex01](docs/exercises/ex01.md)
 * [Ex02](docs/exercises/ex02.md)
 * [Ex03](docs/exercises/ex03.md)
+* [Ex04](docs/exercises/ex04.md)
---
+++ src/test/java/org/pg4200/exercise/ex04/MixedSortTest.java
@@ -0,0 +1,15 @@
+package org.pg4200.exercise.ex04;
+
+import org.pg4200.sorting.MySort;
+import org.pg4200.sorting.SortTestTemplate;
+
+/**
+ * Created by arcuri82 on 21-Aug-17.
+ */
+public class MixedSortTest extends SortTestTemplate {
+
+    @Override
+    protected MySort getInstance() {
+        return new MixedSort(4);
+    }
+}
