010099f0b3db2d0c39af3d29ffef1636d680daa8
arcuri82 <arcuri82@gmail.com>
2017-08-24 10:40:46 +0200

    fixed bugs, added more tests

--- src/main/java/org/pg4200/datastructure/map/tree/BinaryTreeMap.java
+++ src/main/java/org/pg4200/datastructure/map/tree/BinaryTreeMap.java
@@ -5,25 +5,25 @@ package org.pg4200.datastructure.map.tree;
  */
 public class BinaryTreeMap<K extends Comparable<K>, V> implements MyTreeBasedMap<K, V> {
 
-    private class TreeNode {
+    protected class TreeNode {
         K key;
         V value;
         TreeNode left;
         TreeNode right;
     }
 
-    private TreeNode root;
+    protected TreeNode root;
 
-    private int size;
+    protected int size;
 
     @Override
     public void put(K key, V value) {
         root = put(key, value, root);
     }
 
-    private TreeNode put(K key, V value, TreeNode subtree){
+    private TreeNode put(K key, V value, TreeNode subtree) {
 
-        if(subtree == null){
+        if (subtree == null) {
             TreeNode node = new TreeNode();
             node.key = key;
             node.value = value;
@@ -33,13 +33,13 @@ public class BinaryTreeMap<K extends Comparable<K>, V> implements MyTreeBasedMap
 
         int cmp = key.compareTo(subtree.key);
 
-        if(cmp < 0){
-            subtree.left = put(key,value, subtree.left);
+        if (cmp < 0) {
+            subtree.left = put(key, value, subtree.left);
             return subtree;
         }
 
-        if(cmp > 0 ){
-            subtree.right = put(key,value, subtree.right);
+        if (cmp > 0) {
+            subtree.right = put(key, value, subtree.right);
             return subtree;
         }
 
@@ -53,13 +53,7 @@ public class BinaryTreeMap<K extends Comparable<K>, V> implements MyTreeBasedMap
     @Override
     public void delete(K key) {
 
-        TreeNode node = find(key, root);
-        if (node == null) {
-            //nothing to do
-            return;
-        }
-
-        delete(key, node);
+        root = delete(key, root);
     }
 
     private TreeNode delete(K key, TreeNode subtreeRoot) {
@@ -82,17 +76,19 @@ public class BinaryTreeMap<K extends Comparable<K>, V> implements MyTreeBasedMap
 
         assert cmp == 0;
 
+        size--;
+
         if (subtreeRoot.left == null) {
             return subtreeRoot.right;
         }
 
-        if(subtreeRoot.right == null){
+        if (subtreeRoot.right == null) {
             return subtreeRoot.left;
         }
 
         TreeNode tmp = subtreeRoot;
         subtreeRoot = min(tmp.right);
-        subtreeRoot.right =deleteMin(tmp.right);
+        subtreeRoot.right = deleteMin(tmp.right);
         subtreeRoot.left = tmp.left;
 
         return subtreeRoot;
@@ -129,14 +125,14 @@ public class BinaryTreeMap<K extends Comparable<K>, V> implements MyTreeBasedMap
 
     private TreeNode find(K key, TreeNode subtreeRoot) {
 
-        if(subtreeRoot == null){
+        if (subtreeRoot == null) {
             return null;
         }
 
 
         int cmp = key.compareTo(subtreeRoot.key);
 
-        if(cmp == 0){
+        if (cmp == 0) {
             return subtreeRoot;
         } else if (cmp > 0 && subtreeRoot.right != null) {
             //look at greater values in the right subtree
@@ -164,7 +160,7 @@ public class BinaryTreeMap<K extends Comparable<K>, V> implements MyTreeBasedMap
         return depth(root);
     }
 
-    private int depth(TreeNode node) {
+    protected int depth(TreeNode node) {
 
         int leftDepth = 0;
         int rightDepth = 0;
--- src/test/java/org/pg4200/datastructure/map/MyMapTestTemplate.java
+++ src/test/java/org/pg4200/datastructure/map/MyMapTestTemplate.java
@@ -3,6 +3,10 @@ package org.pg4200.datastructure.map;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 import static org.junit.Assert.*;
 
 /**
@@ -10,35 +14,35 @@ import static org.junit.Assert.*;
  */
 public abstract class MyMapTestTemplate {
 
-    protected abstract <K extends Comparable<K>,V> MyMap<K,V>  getInstance();
+    protected abstract <K extends Comparable<K>, V> MyMap<K, V> getInstance();
 
     private MyMap<String, Integer> map;
 
     @Before
-    public void initTest(){
+    public void initTest() {
         map = getInstance();
     }
 
     @Test
-    public void testEmpty(){
+    public void testEmpty() {
 
         assertEquals(0, map.size());
         assertTrue(map.isEmpty());
     }
 
     @Test
-    public void testAddOne(){
+    public void testAddOne() {
 
         int n = map.size();
 
         map.put("foo", 1);
 
-        assertEquals(n +1, map.size());
+        assertEquals(n + 1, map.size());
     }
 
 
     @Test
-    public void testRetrieve(){
+    public void testRetrieve() {
 
         String key = "foo";
         int value = 42;
@@ -53,7 +57,7 @@ public abstract class MyMapTestTemplate {
     }
 
     @Test
-    public void testUpdateOld(){
+    public void testUpdateOld() {
 
         String key = "foo";
         int x = 42;
@@ -70,25 +74,76 @@ public abstract class MyMapTestTemplate {
         assertEquals(y, res);
     }
 
+    @Test
+    public void testDelete(){
+
+        int n = map.size();
+
+        String key = "foo";
+        int x = 42;
+
+        map.put(key, x);
+
+        assertEquals(x, map.get(key).intValue());
+        assertEquals(n+1, map.size());
+
+        map.delete(key);
+
+        assertNull(map.get(key));
+        assertEquals(n, map.size());
+    }
+
 
     @Test
-    public void testAddSeveral(){
+    public void testAddSeveral() {
 
-        map.put("a",0);
-        map.put("a",0);
-        map.put("b",1);
-        map.put("b",1);
-        map.put("c",2);
+        map.put("a", 0);
+        map.put("a", 0);
+        map.put("b", 1);
+        map.put("b", 1);
+        map.put("c", 2);
 
-        map.put("a",0);
-        map.put("a",0);
-        map.put("b",1);
-        map.put("b",1);
-        map.put("c",2);
+        map.put("a", 0);
+        map.put("a", 0);
+        map.put("b", 1);
+        map.put("b", 1);
+        map.put("c", 2);
 
         assertEquals(3, map.size());
         assertEquals(0, map.get("a").intValue());
         assertEquals(1, map.get("b").intValue());
         assertEquals(2, map.get("c").intValue());
+    }
+
+    @Test
+    public void testRandom() {
+
+        int n = 100;
+        List<String> keys = new ArrayList<>(n);
+        for (int i = 0; i < n; i++) {
+            keys.add("" + i);
+        }
+
+        for (int i = 0; i < 10_000; i++) {
+            Collections.shuffle(keys);
+            map = getInstance();
+
+            for (String key : keys) {
+                map.put(key, 0);
+            }
+
+            assertEquals(keys.size(), map.size());
+            for (String key : keys) {
+                assertNotNull(map.get(key));
+            }
+
+            int size = keys.size();
+            for(String key: keys){
+                map.delete(key);
+                assertNull(map.get(key));
+                size--;
+                assertEquals(size, map.size());
+            }
+        }
     }
 }
