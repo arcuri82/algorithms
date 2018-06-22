de646da01177db98f2d2b0268bef400e3f7bb913
arcuri82 <arcuri82@gmail.com>
2018-06-22 10:41:33 +0200

    working on les12

--- lessons/src/main/java/org/pg4200/les12/TextCompression.java
+++ lessons/src/main/java/org/pg4200/les12/TextCompression.java
@@ -5,8 +5,16 @@ package org.pg4200.les12;
  */
 public interface TextCompression {
 
+    /**
+     *  Given an input text, compress it, and return such
+     *  compressed data as an array of bytes
+     */
     byte[] compress(String text);
 
+    /**
+     * Given a previously compressed byte array, decompress it
+     * and return it as a string
+     */
     String decompress(byte[] data);
 
     /**
