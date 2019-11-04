6e0e46d9ca85bfac37e71ed14f5a5fa4aad56f12
arcuri82 <arcuri82@gmail.com>
2019-11-04 13:34:40 +0100

    added clarifications on MyMapBinarySearchTree

--- lessons/src/main/java/org/pg4200/les05/MyMapBinarySearchTree.java
+++ lessons/src/main/java/org/pg4200/les05/MyMapBinarySearchTree.java
@@ -81,6 +81,12 @@ public class MyMapBinarySearchTree<K extends Comparable<K>, V> implements MyMapT
     protected TreeNode delete(K key, TreeNode subtreeRoot) {
 
         if (subtreeRoot == null) {
+            /*
+                This will happen when the key is not found, and we try a
+                recursion on a null node.
+                In this case, the new root of a null substree is still a null
+                subtree, and we can return itself directly (ie null)
+             */
             return null;
         }
 
@@ -101,10 +107,27 @@ public class MyMapBinarySearchTree<K extends Comparable<K>, V> implements MyMapT
             How to delete this node will depend on
             how many children it has
          */
-
         assert cmp == 0;
 
         size--;
+
+        /*
+            What we are going to do here depends on the number of children:
+            0
+            1 (left or right)
+            2
+
+            The (2) is the most complex case.
+            For the (1), just need to check which child is not-null, and that will become
+            the new subtree returned when "this" node is deleted.
+            If both children are missing (ie case 0), then the subtree is just "this" node.
+            Once deleted, what is left is just a null subtree.
+            So we return null.
+            However, we do not need to explicitely have a check like
+            "if(subtreeRoot.left == null && subtreeRoot.right == null) return null"
+            as the following check would give the same result (ie returning null because
+            subtreeRoot.right is null) even in the (0) case.
+         */
 
         if (subtreeRoot.left == null) {
             return subtreeRoot.right;
@@ -117,6 +140,7 @@ public class MyMapBinarySearchTree<K extends Comparable<K>, V> implements MyMapT
         /*
             Both children are present
          */
+        assert subtreeRoot.left != null && subtreeRoot.right != null;
 
         TreeNode tmp = subtreeRoot;
         subtreeRoot = min(tmp.right);
