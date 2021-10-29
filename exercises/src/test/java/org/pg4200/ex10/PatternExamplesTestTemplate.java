66307b225f555e2645c72bcea2687785deb5cf47
Bogdan Marculescu <marculescu.bogdan@gmail.com>
2021-10-29 15:43:40 +0200

    Minor adjustments for Text Search and Regex.

--- exercises/src/test/java/org/pg4200/ex10/PatternExamplesTestTemplate.java
+++ exercises/src/test/java/org/pg4200/ex10/PatternExamplesTestTemplate.java
@@ -84,11 +84,13 @@ public abstract class PatternExamplesTestTemplate {
         assertFalse("".matches(regex));
         assertFalse("Is this an out of season april fools jok".matches(regex));
         assertFalse("Is this an out of season april fools joke".matches(regex));
+        assertFalse("Is this Bogdan's favorite movie?".matches(regex));
         assertFalse("a b c d e f g h i?".matches(regex));
         assertFalse("i S THIS          AN oUT of sEason              APRIL FOOLs joKe?".matches(regex));
         assertFalse("this Is an out of season april fools joke?".matches(regex));
 
         assertTrue("Is this an out of season april fools joke?".matches(regex));
+        assertTrue("Is this an out of season april fools' joke?".matches(regex));
         assertTrue("iS THIS          AN oUT of sEason              APRIL FOOLs joKe?".matches(regex));
     }
 }
